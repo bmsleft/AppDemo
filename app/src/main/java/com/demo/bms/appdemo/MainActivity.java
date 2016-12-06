b8f28c694d52bddd3702e4582cf19a6765a1e07f
bmsleft <bmsleft@163.com>
2016-12-06 20:10:54 +0800

    init project

---
+++ app/src/main/java/com/demo/bms/appdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.demo.bms.appdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("B M S");
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
