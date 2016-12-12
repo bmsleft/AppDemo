19bacc56903b12d4dbde5f78a04cbd4be3542ecb
bianmaosong <bianmaosong@pset.suntec.net>
2016-12-12 21:13:18 +0800

    add viewpager and fragment
    Change-Id: Ib533b4209955160d3b801859979355f96b7cad43

--- app/src/main/java/com/demo/bms/appdemo/MainActivity.java
+++ app/src/main/java/com/demo/bms/appdemo/MainActivity.java
@@ -1,16 +1,55 @@
 package com.demo.bms.appdemo;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private MyFragmentPageAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        mViewPager = (ViewPager) findViewById(R.id.main_page);
 
+        FragmentManager fm = getSupportFragmentManager();
+        mAdapter = new MyFragmentPageAdapter(fm);
+        mViewPager.setAdapter(mAdapter);
     }
 
+
+
+
+    private class MyFragmentPageAdapter extends FragmentStatePagerAdapter {
+        public MyFragmentPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return MyFragment.newInstance(position);
+                case 1:
+                    return MyFragment.newInstance(position);
+                case 2:
+                    return MyFragment.newInstance(position);
+                default:
+                    return null;
+            }
+        }
+
+    }
 }
+
+
---
+++ app/src/main/java/com/demo/bms/appdemo/MyFragment.java
@@ -0,0 +1,42 @@
+package com.demo.bms.appdemo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by bms on 16-12-12.
+ */
+
+public class MyFragment extends Fragment {
+
+    int mNum;
+    public static MyFragment newInstance(int num) {
+        MyFragment fragment = new MyFragment();
+        Bundle args = new Bundle();
+        args.putInt("num", num);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pager_list, null);
+        TextView textView = (TextView) view.findViewById(R.id.fm_text);
+        textView.setText("this fragment is : " + mNum + " \n" + getResources().getString(R.string.my_test));
+        return view;
+    }
+
+
+
+
+}
